981e924a623059c4e0ad941ff0547dcbd3ca90ae
Carlos Eduardo Llacsahuanga <carlosedullacsahuangaabad3@gmail.com>
2025-06-27 02:40:55 -0500

    Update EnvChecker.java

--- src/main/java/com/tienda/tiendabasica2/EnvChecker.java
+++ src/main/java/com/tienda/tiendabasica2/EnvChecker.java
@@ -1,13 +1,1 @@
-import javax.annotation.PostConstruct;
-import org.springframework.stereotype.Component;
 
-@Component
-public class EnvChecker {
-
-  @PostConstruct
-  public void check() {
-    System.out.println(">>>> SPRING_DATASOURCE_URL      = " + System.getenv("SPRING_DATASOURCE_URL"));
-    System.out.println(">>>> SPRING_DATASOURCE_USERNAME = " + System.getenv("SPRING_DATASOURCE_USERNAME"));
-    System.out.println(">>>> SPRING_DATASOURCE_PASSWORD = " + (System.getenv("SPRING_DATASOURCE_PASSWORD") != null ? "***SET***" : null));
-  }
-}
